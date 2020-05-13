9178bc81007d25e8b87238534109f24f3c670500
chris220688 <clion2206@gmail.com>
2020-05-13 20:41:45 +0100

    Changing functional component to class component

--- react/apps/frontend/src/components/References.js
+++ react/apps/frontend/src/components/References.js
@@ -1,88 +1,121 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 
 import { Button, Col, Container, Form, Row } from 'react-bootstrap'
 
 
-export default function References(props) {
+class References extends Component {
 
-	const producerInsertEndpoint = props.producerInsertEndpoint
-	const producerReferencesEndpoint = props.producerReferencesEndpoint
-	const closeReferences = props.closeReferences
-
-	const [data, loading] = useReferences(producerReferencesEndpoint)
-
-	let title = null
-	let date = null
-	let description = null
-	let bookName = null
-	let startingPage = null
-	let endingPage = null
-	let books = []
-	let bookSections = []
-
-	const handleTitleChange = (e) => {
-		title = e.target.value
+	state = {
+		producerInsertEndpoint: this.props.producerInsertEndpoint,
+		producerReferencesEndpoint: this.props.producerReferencesEndpoint,
+		closeReferences: this.props.closeReferences,
+		title: null,
+		date: null,
+		description: null,
+		bookName: null,
+		startingPage: null,
+		endingPage: null,
+		books: [],
+		bookSections: [],
+		loading: true,
+		references: [],
 	}
 
-	const handleDateChange = (e) => {
-		date = e.target.value
-	}
-
-	const handleDescriptionChange = (e) => {
-		description = e.target.value
-	}
-
-	const handleBookChange = (e) => {
-		bookName = e.target.value
-	}
-
-	const handleStartingPageChange = (e) => {
-		startingPage = e.target.value
-	}
-
-	const handleEndingPageChange = (e) => {
-		endingPage = e.target.value
-	}
-
-	const addSection = () => {
-		bookSections.push(
-			{
-				starting_page: startingPage,
-				ending_page: endingPage
-			}
-		)
-		startingPage = null
-		endingPage = null
-	}
-
-	const addBook = () => {
-		books.push(
-			{
-				name: bookName,
-				book_sections: bookSections,
-			}
-		)
-		bookName = null
-		bookSections = []
-	}
-
-	const getValues = () => {
-		console.log(title)
-		console.log(date)
-		console.log(description)
-		console.log(bookName)
-		console.log(books)
-	}
-
-	const addReference = () => {
-		var reference = {
-			title: title,
-			event_date: date,
-			description: description,
-			books: books
+	componentDidMount() {
+		const request = {
+			method: 'GET',
+			credentials: 'include',
 		}
 
-		console.log(reference)
+		fetch(this.state.producerReferencesEndpoint, request)
+		.then(response => response.json())
+		.then(data => {
+			this.setState({
+				references: data['references'],
+				loading: false
+			})
+			console.log(data['references'])
+		})
+		.catch(err => console.log(err))
+	}
+
+	handleTitleChange = (e) => {
+		this.setState({
+			title: e.target.value
+		})
+	}
+
+	handleDateChange = (e) => {
+		this.setState({
+			date: e.target.value
+		})
+	}
+
+	handleDescriptionChange = (e) => {
+		this.setState({
+			description: e.target.value
+		})
+	}
+
+	handleBookChange = (e) => {
+		this.setState({
+			bookName: e.target.value
+		})
+	}
+
+	handleStartingPageChange = (e) => {
+		this.setState({
+			startingPage: e.target.value
+		})
+	}
+
+	handleEndingPageChange = (e) => {
+		this.setState({
+			endingPage: e.target.value
+		})
+	}
+
+	addSection = () => {
+		var bookSections = this.state.bookSections
+		bookSections.push(
+			{
+				starting_page: this.state.startingPage,
+				ending_page: this.state.endingPage,
+			}
+		)
+
+		this.setState({
+			bookSections: bookSections,
+			startingPage: null,
+			endingPage: null
+		})
+
+		console.log(this.state.bookSections)
+	}
+
+	addBook = () => {
+		var books = this.state.books
+		books.push(
+			{
+				name: this.state.bookName,
+				book_sections: this.state.bookSections,
+			}
+		)
+
+		this.setState({
+			books: books,
+			bookName: null,
+			bookSections: []
+		})
+	}
+
+	addReference = () => {
+		var reference = {
+			title: this.state.title,
+			event_date: this.state.date,
+			description: this.state.description,
+			books: this.state.books
+		}
 
 		const putReference = {
 			method: 'PUT',
@@ -90,122 +123,125 @@ export default function References(props) {
 			body: JSON.stringify(reference)
 		}
 
-		fetch(producerInsertEndpoint, putReference)
+		fetch(this.state.producerInsertEndpoint, putReference)
 		.then(response => response.json())
-		.then(data => console.log(data))
+		.then(data => {
+			var references = this.state.references
+			references.push(reference)
+			this.setState({
+				references: references
+			})
+		})
 		.catch(err => console.log(err))
 	}
 
-	return (
-		<section>
-			<Container fluid>
-				<Row>
-					<Col></Col>
-					<Col></Col>
-					<Col className="text-right">
-						<Button variant="secondary" onClick={closeReferences}>
-							Back
-						</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<Container fluid>
-							<Row>
-								<Col>
-									<Form>
-										<Form.Group controlId="reference.Title">
-											<Form.Label>Event title</Form.Label>
-											<Form.Control type="text" onChange={handleTitleChange}/>
+	render() {
+		return (
+			<section>
+				<Container fluid>
+					<Row>
+						<Col></Col>
+						<Col></Col>
+						<Col className="text-right">
+							<Button variant="secondary" onClick={this.props.closeReferences}>
+								Back
+							</Button>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Container fluid>
+								<Row>
+									<Col>
+										<Form>
+											<Form.Group controlId="reference.Title">
+												<Form.Label>Event title</Form.Label>
+												<Form.Control type="text" onChange={this.handleTitleChange}/>
+											</Form.Group>
+											<Form.Group controlId="reference.Date">
+												<Form.Label>Event date</Form.Label>
+												<Form.Control type="datetime-local" onChange={this.handleDateChange}/>
+											</Form.Group>
+											<Form.Group controlId="reference.Description">
+												<Form.Label>Event description</Form.Label>
+												<Form.Control as="textarea" rows="5" onChange={this.handleDescriptionChange}/>
+											</Form.Group>
+										</Form>
+									</Col>
+								</Row>
+								<Row  style={{border: '1px solid black' }}>
+									<Col>
+										<Form.Group controlId="reference.Book">
+											<Form.Label>Book name</Form.Label>
+											<Form.Control type="text" onChange={this.handleBookChange}/>
 										</Form.Group>
-										<Form.Group controlId="reference.Date">
-											<Form.Label>Event date</Form.Label>
-											<Form.Control type="date" onChange={handleDateChange}/>
-										</Form.Group>
-										<Form.Group controlId="reference.Description">
-											<Form.Label>Event description</Form.Label>
-											<Form.Control as="textarea" rows="5" onChange={handleDescriptionChange}/>
-										</Form.Group>
-									</Form>
-								</Col>
-							</Row>
-							<Row  style={{border: '1px solid black' }}>
-								<Col>
-									<Form.Group controlId="reference.Book">
-										<Form.Label>Book name</Form.Label>
-										<Form.Control type="text" onChange={handleBookChange}/>
-									</Form.Group>
-									<Form.Group controlId="reference.StartingPage">
-										<Form.Label>Starting page</Form.Label>
-										<Form.Control type="number" onChange={handleStartingPageChange}/>
-									</Form.Group>
-									<Form.Group controlId="reference.EndingPage">
-										<Form.Label>Ending page</Form.Label>
-										<Form.Control type="number" onChange={handleEndingPageChange}/>
-									</Form.Group>
-									<Button variant="secondary" onClick={addSection}>
-										Add section
-									</Button>
-									<Button variant="secondary" onClick={addBook}>
-										Add book
-									</Button>
-								</Col>
-							</Row>
-							<Row>
-							<ul>
-								{books.map(({ name }, index) => (
-									<li key={index}>
-										{name}
-									</li>
-								))}
-							</ul>
-							</Row>
-							<Row className="text-right">
-								<Col>
-									<Button variant="secondary" onClick={addReference}>
-										Add reference
-									</Button>
-								</Col>
-							</Row>
-						</Container>
-					</Col>
-					<Col>
-						{loading ? (
-							"Loading..."
-						) : (
-							<ul>
-								{data.references.map(({ title }, index) => (
-									<li key={index}>
-										{title}
-									</li>
-								))}
-							</ul>
-						)}
-					</Col>
-				</Row>
-			</Container>
-		</section>
-	);
+										<Container fluid>
+											<Row>
+												<Col>
+												<Form.Group controlId="reference.StartingPage">
+													<Form.Label>Starting page</Form.Label>
+													<Form.Control type="number" onChange={this.handleStartingPageChange}/>
+												</Form.Group>
+												</Col>
+												<Col>
+												<Form.Group controlId="reference.EndingPage">
+													<Form.Label>Ending page</Form.Label>
+													<Form.Control type="number" onChange={this.handleEndingPageChange}/>
+												</Form.Group>
+												</Col>
+											</Row>
+										</Container>
+										<Button variant="secondary" onClick={this.addSection}>
+											Add section
+										</Button>
+											<ul>
+												{this.state.bookSections.map(({ starting_page, ending_page }, index) => (
+													<li key={index}>
+														{starting_page}-{ending_page}
+													</li>
+												))}
+											</ul>
+										<Button variant="secondary" onClick={this.addBook}>
+											Add book
+										</Button>
+									</Col>
+								</Row>
+								<Row>
+								<ul>
+									{this.state.books.map(({ name }, index) => (
+										<li key={index}>
+											{name}
+										</li>
+									))}
+								</ul>
+								</Row>
+								<Row className="text-right">
+									<Col>
+										<Button variant="secondary" onClick={this.addReference}>
+											Add reference
+										</Button>
+									</Col>
+								</Row>
+							</Container>
+						</Col>
+						<Col>
+							{this.state.loading ? (
+								"Loading..."
+							) : (
+								<ul>
+									{this.state.references.map(({ title, description }, index) => (
+										<li key={index}>
+											{title}
+										</li>
+									))}
+								</ul>
+							)}
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		);
+	}
 }
 
-
-function useReferences(url) {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		async function fetchUrl() {
-			const request = {
-				method: 'GET',
-				credentials: 'include',
-			}
-			const response = await fetch(url, request);
-			const json = await response.json();
-			setData(json);
-			setLoading(false);
-		}
-
-		fetchUrl();
-	}, [url]);
-	return [data, loading];
-}
+export default References;

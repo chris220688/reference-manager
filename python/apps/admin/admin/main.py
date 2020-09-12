bf3b0f491ee6c929f47c05a56a4bc88d1180e7ee
chris220688 <clion2206@gmail.com>
2020-09-12 16:34:55 +0100

    DEV-30: Allowing CORS for admin

--- python/apps/admin/admin/main.py
+++ python/apps/admin/admin/main.py
@@ -61,17 +61,17 @@ access_token_cookie_scheme = auth_schemes.AccessTokenCookieBearer()
 
 
 # Allow CORS only locally
-if config.LOCAL_DEPLOYMENT:
-	origins = [
-		"http://localhost:3000",
-	]
-	app.add_middleware(
-		CORSMiddleware,
-		allow_origins=origins,
-		allow_credentials=True,
-		allow_methods=["*"],
-		allow_headers=["*"],
-	)
+# if config.LOCAL_DEPLOYMENT:
+origins = [
+	"http://localhost:3000",
+]
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
